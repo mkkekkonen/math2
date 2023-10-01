7628eb102cacefc422d69de372e74a86849a012a
Maija Kekkonen <mkkekkonen@protonmail.com>
2023-10-01 22:24:58 +0300

    Move backend files down one directory level - add .env and .pyc to .gitignore

--- backend/math2/settings.py
+++ backend/math2/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,9 +80,12 @@ WSGI_APPLICATION = 'math2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'math2',
+        'USER': 'math2',
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': 'localhost',
+    },
 }
 
 
