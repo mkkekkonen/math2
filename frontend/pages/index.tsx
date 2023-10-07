99b33a1517dbf5a8313729651494f7752f919f99
Maija Kekkonen <mkkekkonen@protonmail.com>
2023-10-08 00:30:16 +0300

    Add getServerSideProps function to fetch categories and pages - display categories and pages as lists on the index page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,26 @@
-const Index = () => (
+export const getServerSideProps = (async context => {
+  const categoryRes = await fetch('http://127.0.0.1:8000/categories/');
+  const pageRes = await fetch('http://127.0.0.1:8000/pages/');
+  const categories = await categoryRes.json();
+  const pages = await pageRes.json();
+  return { props: { categories, pages } };
+});
+
+const Index = ({ categories, pages }: { categories: any[], pages: any[] }) => (
   <div>
     <h1>Hello world</h1>
+    <h3>Categories</h3>
+    <ul>
+      {categories && categories.map(category => (
+        <li key={category.id}>{category.localization_key}</li>
+      ))}
+    </ul>
+    <h3>Pages</h3>
+    <ul>
+      {pages && pages.map(page => (
+        <li key={page.id}>{page.localization_key}</li>
+      ))}
+    </ul>
   </div>
 );
 
