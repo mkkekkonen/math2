a4c07c6dc34f37ef97cf1e58e2a46da578694467
Maija Kekkonen <mkkekkonen@protonmail.com>
2025-11-19 08:37:56 +0200

    Wrap ssl_disabled in an "ssl" dictionary

--- backend/math2/settings_cloud.py
+++ backend/math2/settings_cloud.py
@@ -12,7 +12,9 @@ DATABASES = {
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
-            'ssl_disabled': False,
+            'ssl': {
+                'ssl_disabled': False,
+            }
         }
     },
 }
