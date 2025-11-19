bb896b086bc77cb77a6602c45c0164db3c9eca9d
Maija Kekkonen <mkkekkonen@protonmail.com>
2025-11-19 08:32:56 +0200

    Use TCP database socket and attempt to enable SSL explicitly in Django settings

--- backend/math2/settings_cloud.py
+++ backend/math2/settings_cloud.py
@@ -12,6 +12,7 @@ DATABASES = {
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
+            'ssl_disabled': False,
         }
     },
 }
