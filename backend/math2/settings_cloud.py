2c3cd539d384007fd56a06a8295d251e8c826b35
Maija Kekkonen <mkkekkonen@protonmail.com>
2025-11-19 07:42:03 +0200

    Add initial Docker/cloud support for backend

---
+++ backend/math2/settings_cloud.py
@@ -0,0 +1,36 @@
+from math2.settings import *
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DJANGO_DB_USER'),
+        'PASSWORD': env('DJANGO_DB_PASSWORD'),
+        'HOST': env('DJANGO_DB_SOCKET'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
+    },
+}
+
+ALLOWED_HOSTS = ('.mathvisualized.dev',)
+
+STATIC_ROOT = '/app/static/'
+
+LOCALE_PATHS = (
+    '/app/locale/',
+)
+
+STATIC_URL = '/api/static/'
+
+FORCE_SCRIPT_NAME = '/api'
+
+CSRF_COOKIE_SECURE = True
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://www.mathvisualized.dev', 'https://mathvisualized.dev']
+
+SESSION_COOKIE_SECURE = True
