4bfa3366442c80f85d1f98a2294dbde66a0071df
Maija Kekkonen <mkkekkonen@protonmail.com>
2025-11-19 09:10:49 +0200

    Fix SSL keyword argument

--- backend/math2/settings_cloud.py
+++ backend/math2/settings_cloud.py
@@ -13,7 +13,7 @@ if env('GH_ACTIONS') == 'true':
             'OPTIONS': {
                 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
                 'charset': 'utf8mb4',
-                'use_ssl': False,
+                'ssl_disabled': True,
                 'auth_plugin': 'mysql_native_password',
             }
         },
