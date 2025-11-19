96de112ae105640527e9bb2293f1b1fc15b81cbf
Maija Kekkonen <mkkekkonen@protonmail.com>
2025-11-19 08:02:49 +0200

    Edit secret / environment variable names

--- backend/math2/settings_cloud.py
+++ backend/math2/settings_cloud.py
@@ -5,10 +5,10 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('GCP_DB_NAME'),
-        'USER': env('GCP_DJANGO_DB_USER'),
-        'PASSWORD': env('GCP_DJANGO_DB_PASSWORD'),
-        'HOST': env('GCP_DJANGO_DB_SOCKET'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DJANGO_DB_USER'),
+        'PASSWORD': env('DJANGO_DB_PASSWORD'),
+        'HOST': env('DJANGO_DB_HOST'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
